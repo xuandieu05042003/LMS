bb639abcac2bf519117dbbf552ee40aa3d5c13f8
xuandieu05042003 <dieunx0504@gamil.com>
2024-04-01 17:46:11 +0700

    lecturer

--- LMS/Models/Lecturer.cs
+++ LMS/Models/Lecturer.cs
@@ -9,8 +9,10 @@ namespace LMS.Models
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
         public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
 
     }
 }
--- LMS/obj/Debug/net8.0/LMS.AssemblyInfo.cs
+++ LMS/obj/Debug/net8.0/LMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f963476d4b4d16f7509f8b3d17be05fb4198bb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655d1b9d78623109cb95102551fddc9a10e59f17")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
