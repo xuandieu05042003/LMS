29f84d2fa20d90bd4200861214c9b81feff2f745
xuandieu05042003 <dieunx0504@gamil.com>
2024-04-02 16:21:46 +0700

    dieu

---
+++ LMS/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LMS.Models;
+
+namespace LMS.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Login model)
+        {
+            if (model.Username == "admin" && model.Password == "adminpassword")
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+            else if (model.Username == "lecturer" && model.Password == "lecturerpassword")
+            {
+                return RedirectToAction("Index", "Lecturer");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid username or password");
+                return View(model);
+            }
+        }
+    }
+}
