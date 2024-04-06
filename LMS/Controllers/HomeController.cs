3a50485d0b521fa241ac65b3b2d3fca93d4d950d
dinhthai0903 <thainguyen1192003@gmail.com>
2024-04-06 11:09:22 +0700

    upload new

--- LMS/Controllers/HomeController.cs
+++ LMS/Controllers/HomeController.cs
@@ -85,59 +85,56 @@ namespace LMS.Controllers
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> Login(string email, string password)
-        {
-            try
-            {
-                var client = new MongoClient("mongodb+srv://dieunxbd00122:dieu050403@lms.f19fpne.mongodb.net/");
-                var database = client.GetDatabase("universityDtabase");
+		public async Task<IActionResult> Login(string email, string password)
+		{
+			try
+			{
+				var client = new MongoClient("mongodb+srv://dieunxbd00122:dieu050403@lms.f19fpne.mongodb.net/");
+				var database = client.GetDatabase("universityDtabase");
 
-                // Kiểm tra trong bảng admin
-                var adminCollection = database.GetCollection<Admin>("admin");
-                var adminFilter = Builders<Admin>.Filter.Where(x => x.Email == email && x.Password == password);
-                var adminResult = await adminCollection.Find(adminFilter).FirstOrDefaultAsync();
-                if (adminResult != null)
-                {
-                    // Nếu tài khoản là admin, chuyển hướng đến trang index của admin
-                    return RedirectToAction("IndexLoginAdmin", "Home");
-                }
+				// Kiểm tra trong bảng admin
+				var adminCollection = database.GetCollection<Admin>("admin");
+				var adminFilter = Builders<Admin>.Filter.Where(x => x.Email == email && x.Password == password);
+				var adminResult = await adminCollection.Find(adminFilter).FirstOrDefaultAsync();
+				if (adminResult != null)
+				{
+					// Nếu tài khoản là admin, chuyển hướng đến trang index của admin
+					return RedirectToAction("IndexLoginAdmin", "Home");
+				}
 
-                // Kiểm tra trong bảng lecture nếu không tìm thấy trong bảng admin
-                var lecturerCollection = database.GetCollection<Lecturer>("lecturer");
-                var lecturerFilter = Builders<Lecturer>.Filter.Where(x => x.Email == email && x.Password == password);
-                var lecturerResult = await lecturerCollection.Find(lecturerFilter).FirstOrDefaultAsync();
-                if (lecturerResult != null)
-                {
-                    // Nếu tài khoản là lecture, chuyển hướng đến trang index của lecture
-                    return RedirectToAction("IndexLoginLecture", "Home");
-                }
+				// Kiểm tra trong bảng lecture nếu không tìm thấy trong bảng admin
+				var lecturerCollection = database.GetCollection<Lecturer>("lecturer");
+				var lecturerFilter = Builders<Lecturer>.Filter.Where(x => x.Email == email && x.Password == password);
+				var lecturerResult = await lecturerCollection.Find(lecturerFilter).FirstOrDefaultAsync();
+				if (lecturerResult != null)
+				{
+					// Nếu tài khoản là lecture, chuyển hướng đến trang index của lecture
+					return RedirectToAction("IndexLoginLecture", "Home");
+				}
 
-                // Kiểm tra trong bảng student nếu không tìm thấy trong bảng admin và lecture
-                var studentCollection = database.GetCollection<Student>("student");
-                var studentFilter = Builders<Student>.Filter.Where(x => x.Email == email && x.Password == password);
-                var studentResult = await studentCollection.Find(studentFilter).FirstOrDefaultAsync();
-                if (studentResult != null)
-                {
-                    // Nếu tài khoản là student, chuyển hướng đến trang index của student
-                    return RedirectToAction("IndexLoginUser", "Home");
-                }
+				// Kiểm tra trong bảng student nếu không tìm thấy trong bảng admin và lecture
+				var studentCollection = database.GetCollection<Student>("student");
+				var studentFilter = Builders<Student>.Filter.Where(x => x.Email == email && x.Password == password);
+				var studentResult = await studentCollection.Find(studentFilter).FirstOrDefaultAsync();
+				if (studentResult != null)
+				{
+					// Nếu tài khoản là student, chuyển hướng đến trang index của student
+					return RedirectToAction("IndexLoginUser", "Home");
+				}
 
-                // Nếu không tìm thấy tài khoản trong bất kỳ bảng nào, hiển thị thông báo lỗi
-                ViewBag.ErrorMessage = "Invalid email or password.";
-                return View(); // Trả về view đăng nhập với thông báo lỗi
-            }
-            catch (Exception ex)
-            {
-                // Xử lý ngoại lệ
-                Console.WriteLine($"An error occurred: {ex.Message}");
-                return View(); // Trả về view đăng nhập với thông báo lỗi
-            }
-        }
+				// Nếu không tìm thấy tài khoản trong bất kỳ bảng nào, hiển thị thông báo lỗi
+				ViewBag.ErrorMessage = "Invalid email or password.";
+				return View(); // Trả về view đăng nhập với thông báo lỗi
+			}
+			catch (Exception ex)
+			{
+				// Xử lý ngoại lệ
+				Console.WriteLine($"An error occurred: {ex.Message}");
+				return View(); // Trả về view đăng nhập với thông báo lỗi
+			}
+		}
 
-
-
-
-        public IActionResult Logout()
+		public IActionResult Logout()
         {
 			// Chuyển hướng đến trang đăng nhập
 			return RedirectToAction("Login");
--- LMS/obj/Debug/net8.0/LMS.AssemblyInfo.cs
+++ LMS/obj/Debug/net8.0/LMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922dce873f77cfd582a3e004ea602f37e816c188")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2280f2e53d17c15593ab316085bf7aff8fa28c75")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
