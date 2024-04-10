51534952d4ed5e451cc6e3b3235fadb7479c07ec
xuandieu05042003 <dieunx0504@gamil.com>
2024-04-10 15:42:01 +0700

    edit page home

--- LMS/obj/Debug/net8.0/LMS.AssemblyInfo.cs
+++ LMS/obj/Debug/net8.0/LMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07e1d9a91275fcf593e7d1df7d8b53b757079f75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e4ba22530545d058be0d46b29f8fc78e38f84cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
