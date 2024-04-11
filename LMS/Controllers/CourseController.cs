eb8325cd6d6718d1eab8b8237df602d0e0dc1af7
xuandieu05042003 <dieunx0504@gamil.com>
2024-04-11 12:14:35 +0700

    update for courses

--- LMS/Controllers/CourseController.cs
+++ LMS/Controllers/CourseController.cs
@@ -70,6 +70,67 @@ namespace LMS.Controllers
             var course = table.Find(FilterDefinition<Course>.Empty).ToList();
             return View(course);
         }
+        public async Task<IActionResult> MyCourses()
+        {
+            // session for admin
+            var adminId = HttpContext.Session.GetString("AdminId");
+            var adminName = HttpContext.Session.GetString("AdminName");
+            var adminRole = HttpContext.Session.GetString("AdminRole");
+            var adminEmail = HttpContext.Session.GetString("AdminEmail");
+            var adminImage = HttpContext.Session.GetString("AdminImage");
+
+            // session for lecture
+            var lecturerId = HttpContext.Session.GetString("LecturerId");
+            var lecturerName = HttpContext.Session.GetString("LecturerName");
+            var lecturerRole = HttpContext.Session.GetString("LecturerRole");
+            var lecturerEmail = HttpContext.Session.GetString("LecturerEmail");
+            var lecturerImage = HttpContext.Session.GetString("LecturerImage");
+
+            // session for student
+            var studentId = HttpContext.Session.GetString("StudentId");
+            var studentName = HttpContext.Session.GetString("StudentName");
+            var studentRole = HttpContext.Session.GetString("StudentRole");
+            var studentEmail = HttpContext.Session.GetString("StudentEmail");
+            var studentImage = HttpContext.Session.GetString("StudentImage");
+
+            // 
+            if (!string.IsNullOrEmpty(adminId) && !string.IsNullOrEmpty(adminName) && !string.IsNullOrEmpty(adminRole) && !string.IsNullOrEmpty(adminEmail) && !string.IsNullOrEmpty(adminImage))
+            {
+                ViewBag.Id = adminId;
+                ViewBag.Name = adminName;
+                ViewBag.Role = adminRole;
+                ViewBag.Email = adminEmail;
+                ViewBag.Image = adminImage;
+            }
+            else if (!string.IsNullOrEmpty(lecturerId) && !string.IsNullOrEmpty(lecturerName) && !string.IsNullOrEmpty(lecturerRole) && !string.IsNullOrEmpty(lecturerEmail) && !string.IsNullOrEmpty(lecturerImage))
+            {
+                ViewBag.Id = lecturerId;
+                ViewBag.Name = lecturerName;
+                ViewBag.Role = lecturerRole;
+                ViewBag.Email = lecturerEmail;
+                ViewBag.Image = lecturerImage;
+            }
+            else if (!string.IsNullOrEmpty(studentId) && !string.IsNullOrEmpty(studentName) && !string.IsNullOrEmpty(studentRole) && !string.IsNullOrEmpty(studentEmail) && !string.IsNullOrEmpty(studentImage))
+            {
+                ViewBag.Id = studentId;
+                ViewBag.Name = studentName;
+                ViewBag.Role = studentRole;
+                ViewBag.Email = studentEmail;
+                ViewBag.Image = studentImage;
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid session";
+            }
+            var database = client.GetDatabase("universityDtabase");
+            var courseCollection = database.GetCollection<Course>("course");
+
+            // Truy vấn các khóa học thuộc về giảng viên này dựa trên LecturerId
+            var courseFilter = Builders<Course>.Filter.Eq(x => x.LecturerId, lecturerId);
+            var courses = await courseCollection.Find(courseFilter).ToListAsync();
+
+            return View(courses);
+        }
         public IActionResult CourseDetail()
         {
 			// session for admin
@@ -261,7 +322,7 @@ namespace LMS.Controllers
             var database = client.GetDatabase("universityDtabase");
             var table = database.GetCollection<Course>("course");
             table.ReplaceOne(c => c.Id == course.Id, course);
-            return RedirectToAction("Index");
+            return RedirectToAction("MyCourses");
         }
 
         public ActionResult Details(String id)
@@ -344,7 +405,7 @@ namespace LMS.Controllers
             var database = client.GetDatabase("universityDtabase");
             var table = database.GetCollection<Course>("course");
             table.DeleteOne(c => c.Id == course.Id);
-            return RedirectToAction("Index");
+            return RedirectToAction("MyCourses");
         }
         public IActionResult Categories()
         {
@@ -454,8 +515,12 @@ namespace LMS.Controllers
 			}
 			var database = client.GetDatabase("universityDtabase");
             var table = database.GetCollection<Course>("course");
-            var course = table.Find(_ => true).FirstOrDefault();
-
+            var course = table.Find(c => c.Id == id).FirstOrDefault();
+			if (course == null)
+			{
+                return NotFound();
+            }	
+			
             return View(course);
         }
         public IActionResult Allcourse()
